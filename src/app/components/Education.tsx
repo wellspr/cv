c3063ee6776c66877d0f241353d0131dffc90869
Paulo Wells <paulowells@gmail.com>
2025-02-20 01:29:34 -0300

    updated css class

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -7,10 +7,10 @@ export const EducationComponent = (prop: EducationType) => {
             <h4 className="education__institution">{prop.institution}</h4>
             <p className="education__location">{prop.location.city}, {prop.location.state} - {prop.location.country}</p>
             <div className="education__dates">
-                <span className="job__date job__start job__start__month">{prop.month_start}</span>
-                <span className="job__date job__start job__start__year">{prop.year_start}</span>
-                <span className="job__date job__finish job__finish__month">{prop.month_finish}</span>
-                <span className="job__date job__finish job__finish__year">{prop.year_finish}</span>
+                <span className="date date__start date__start__month">{prop.month_start}</span>
+                <span className="date date__start date__start__year">{prop.year_start}</span>
+                <span className="date date__finish date__finish__month">{prop.month_finish}</span>
+                <span className="date date__finish date__finish__year">{prop.year_finish}</span>
             </div>
         </div>
     );
--- src/app/components/Job.tsx
+++ src/app/components/Job.tsx
@@ -7,10 +7,10 @@ export const JobComponent = (job: JobType) => {
             <h4 className="job__company">{job.company}</h4>
             <p className="job__location">{job.location.city}, {job.location.state} - {job.location.country}</p>
             <div className="job__dates">
-                <span className="job__date job__start job__start__month">{job.start_month}</span>
-                <span className="job__date job__start job__start__year">{job.start_year}</span>
-                <span className="job__date job__finish job__finish__month">{job.finish_month}</span>
-                <span className="job__date job__finish job__finish__year">{job.finish_year}</span>
+                <span className="date date__start date__start__month">{job.start_month}</span>
+                <span className="date date__start date__start__year">{job.start_year}</span>
+                <span className="date date__finish date__finish__month">{job.finish_month}</span>
+                <span className="date date__finish date__finish__year">{job.finish_year}</span>
             </div>
             <p className="job__activities">{job.activities}</p>
         </div>
